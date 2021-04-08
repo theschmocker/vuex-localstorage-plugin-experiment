8ecc6067607ed9981c8e6873b8daf904e9f0196e
theschmocker <jacobcs1996@gmail.com>
2021-04-08 12:47:05 -0500

    add early undefined checks

--- src/create-local-storage-plugin.ts
+++ src/create-local-storage-plugin.ts
@@ -7,9 +7,14 @@ export function createLocalStoragePlugin<S>(stateMap: LocalStoreStateMap<S>, opt
     // Populate store with existing values in storage
     for (const key of Object.keys(stateMap) as (keyof typeof stateMap)[]) {
       const field = stateMap[key];
+
+      if (field == null) { // appease TypeScript
+        continue;
+      }
+
       if (typeof field === 'boolean') {
         store.state[key] = storage.getItem(key) as unknown as S[keyof S] ?? store.state[key];
-      } else if (field) {
+      } else {
         const serialized = storage.getItem(key);
         if (serialized != null) {
           store.state[key] = field.deserialize(serialized);
@@ -22,9 +27,14 @@ export function createLocalStoragePlugin<S>(stateMap: LocalStoreStateMap<S>, opt
         // Assuming for now that the mutation name matches the state field name
         if (mutation.type === key) {
           const field = stateMap[key];
+
+          if (field == null) { // appease TypeScript
+            continue;
+          }
+
           if (typeof field === 'boolean') {
             storage.setItem(key, state[key] as unknown as string)
-          } else if (field) {
+          } else {
             storage.setItem(key, field.serialize(state))
           }
         }
